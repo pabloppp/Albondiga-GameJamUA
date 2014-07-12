27a4541cd37093fe28778d05ba41685776099c25
dank0ne <dank0ner@gmail.com>
2014-07-12 20:23:12 +0200

    Script de generación de oasis e ilusión

---
+++ Assets/scripts/Move.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+	public CharacterController controller;
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.W))
+			controller.Move (Vector3.forward);
+		if (Input.GetKey (KeyCode.S))
+			controller.Move (-Vector3.forward);
+		if (Input.GetKey (KeyCode.D))
+			controller.Move (Vector3.right);
+		if (Input.GetKey (KeyCode.A))
+			controller.Move (-Vector3.right);
+	}
+}
