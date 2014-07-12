248f9d01ccd222f088d990d9cc715cfec01d7aca
Roxanne Lopez van Dooren <roxannelopez6@hotmail.com>
2014-07-12 20:21:48 +0200

    Added BIRD with its behavior, and imported TOON SHADES. Modified all the materials.

---
+++ Assets/scripts/birdBehavior.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class birdBehavior : MonoBehaviour {
+	
+	public float speed = 100;
+	public float rotSpeed = 0f;
+	public float mediumHeight = 10;
+	public float heightDif = 5;
+	public float heightFq = 5;
+
+
+	// Use this for initialization
+
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(-Vector3.forward*Time.deltaTime*speed);
+		Vector3 tempPos = transform.position;
+		tempPos.y = mediumHeight+Mathf.Cos(Time.deltaTime*heightFq)*heightDif;
+		transform.position = tempPos;
+		transform.Rotate(Vector3.up*Time.deltaTime*rotSpeed);
+	}
+}
--- Assets/scripts/playerDrinks.cs
+++ Assets/scripts/playerDrinks.cs
@@ -8,6 +8,9 @@ public class playerDrinks : MonoBehaviour {
 	private bool detectingOasis = false;
 	public float oasisWater = 35f;
 
+	public Font f;
+
+
 	// Use this for initialization
 	void Start () {
 	
@@ -20,13 +23,22 @@ public class playerDrinks : MonoBehaviour {
 	}
 
 	void OnGUI(){
+		if (!f) {
+			Debug.LogError("No font found, assign one in the inspector.");
+			return;
+		}
+
+		GUIStyle myStyle = new GUIStyle(GUI.skin.label);
+		GUI.skin.font = f;
+		myStyle.fontSize = 25;
+		myStyle.normal.textColor = Color.black;
 		if(detectingOasis && !playerDrinking){		
-			GUI.Label (new Rect (Screen.width/2-50,Screen.height*0.8f,200,50), "Press SPACE to drink");
+			GUI.Label (new Rect (Screen.width/2-125,Screen.height*0.8f,250,50), "Press SPACE to drink", myStyle);
 		}
 	}
 
 	void OnTriggerStay(Collider other){
-		//print ("touching...");
+		print ("touching...");
 		if(other.gameObject.tag == "water"){
 			detectingOasis = true;
 			if(Input.GetKeyDown(drinkKey) && !playerDrinking){
